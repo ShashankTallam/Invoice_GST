051da0316441e93200cff8c2f8231fc7f7140ff3
ShashankTallam <shashanktj22@gmail.com>
2025-07-08 18:45:45 +0530

    my first

---
+++ backend/app/__init__.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_session import Session
+from flask_bcrypt import Bcrypt
+from app.config import ApplicationConfig
+from .extensions import db
+from app.models import User, UserRole
+
+bcrypt = Bcrypt()
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Load config
+    app.config.from_object(ApplicationConfig)
+    app.config.update(ENV='development')
+
+    # Initialize DB first
+    db.init_app(app)
+
+    # Set session DB connection AFTER db.init_app()
+    app.config['SESSION_SQLALCHEMY'] = db
+    app.config['CORS_HEADERS'] = 'Content-Type'
+
+    # CORS and session setup
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+    Session(app)
+
+    # Init bcrypt
+    bcrypt.init_app(app)
+
+    # DB setup and admin user creation
+    with app.app_context():
+        db.create_all()
+        initialize_database()
+
+    # Register all blueprints
+    register_blueprints(app)
+
+    return app
+
+def register_blueprints(app):
+    from app.auth.authentication import authentication_bp
+    from app.routes.preprocessing import preprocessing_bp
+    from app.routes.organization import organizations_bp
+    from app.routes.data_routes import getData_bp
+    from app.ocr.tesseractOCR import tesseract_bp
+    from app.ocr.paddleOCR import paddleocr_bp
+    from app.api.companyAPI import companyAPI_bp
+    from app.routes.main_routes import main_bp
+
+    app.register_blueprint(authentication_bp)
+    app.register_blueprint(preprocessing_bp)
+    app.register_blueprint(organizations_bp)
+    app.register_blueprint(getData_bp)
+    app.register_blueprint(tesseract_bp)
+    app.register_blueprint(paddleocr_bp)
+    app.register_blueprint(companyAPI_bp)
+    app.register_blueprint(main_bp)
+
+def initialize_database():
+    admin_user = User.query.filter_by(email='admin').first()
+    if not admin_user:
+        hashed_password = bcrypt.generate_password_hash('admin').decode('utf-8')
+        admin_user = User(email='admin', name='Admin', password=hashed_password)
+        admin_user.role = UserRole.ADMIN
+        db.session.add(admin_user)
+        db.session.commit()
---
+++ backend/app/config.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Language used for invoice OCR
+INVOICE_LANG = "slk"
+
+# Backend URL (for use by frontend if needed)
+BACKEND_URL = "http://localhost:5000"
+
+
+class ApplicationConfig:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///local.db")
+    SESSION_TYPE = "sqlalchemy"
+    SESSION_PERMANENT = True
